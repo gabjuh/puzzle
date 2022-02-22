b2adec5a24d1832b0ba3f67d9ad94fde653b4a5d
gabjuh <adgbe@yahoo.de>
2022-02-22 15:08:03 +0100

    created index.js with hello world message

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+// import Puzzle from './components/Puzzle'
+
+ReactDOM.render(<h2>Hello World</h2>, document.querySelector('#app')) //<Puzzle />
